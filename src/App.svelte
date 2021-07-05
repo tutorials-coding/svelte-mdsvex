39e459444ee5ae51e8b45be4435a30c4e4f5342d
Vadim <korobeinikovv@mail.ru>
2021-07-05 23:26:33 +0300

    Add code highlight

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+	import '../node_modules/prismjs/themes/prism.css'
+	// import '../node_modules/prismjs/themes/prism-funky.css'
+	// import '../node_modules/prismjs/themes/prism-twilight.css'
   import CounterDocs from './components/Counter/Counter.svx'
 </script>
 
